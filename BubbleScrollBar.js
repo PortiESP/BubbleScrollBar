6394fcf17359779187fc1f2e3e0d8fab6ffc9105
Porti <elp0rti201@gmail.com>
2022-07-15 00:18:12 +0200

    removed auto update hash on scroll

--- BubbleScrollBar.js
+++ BubbleScrollBar.js
@@ -78,7 +78,6 @@ export default function BubbleScrollBar(props){
 
     React.useEffect( ()=>{
         resetTimer()
-        location.hash = scrollData.focus
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [scrollData.focus])
